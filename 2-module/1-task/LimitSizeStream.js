07f90e58a8fb8ee774123e08584bb49b87590b6e
Maksim Korostelev <maksim@voidstudio.ru>
2019-08-01 21:32:03 +0300

    decision

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -4,10 +4,18 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit || 1024;
+    this.size = 0;
+    console.log(options);
   }
 
   _transform(chunk, encoding, callback) {
-
+    this.size += Buffer.byteLength(chunk, encoding);
+    console.log(this.size);
+    if (this.size >= this.limit) {
+      callback(new LimitExceededError(), chunk);
+    }
+    callback(null, chunk);
   }
 }
 
